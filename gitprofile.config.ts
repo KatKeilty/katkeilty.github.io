62bac61d602aa0294c7fa1cca7b48f603df26810
Kat Keilty <85399179+KatKeilty@users.noreply.github.com>
2025-11-08 11:05:20 +0000

    path

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -6,7 +6,7 @@ const CONFIG = {
     username: 'KatKeilty', // Your GitHub org/user name. (This is the only required config)
   },
 
-  base: '/gitprofile/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
